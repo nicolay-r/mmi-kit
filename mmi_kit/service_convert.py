cd3d676a1ad4f3314185cdfa57e1c9242e1fa99f
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-03-10 15:49:27 +0000

    Fixed problem with assigning the wrong tag

--- mmi_kit/service_convert.py
+++ mmi_kit/service_convert.py
@@ -30,10 +30,10 @@ def nifti_to_dicom(patient_id, patient_name, modality, image_matrix, age,
     ds.SOPInstanceUID = pydicom.uid.generate_uid() if sop_instance_uid is None else sop_instance_uid
     ds.StudyInstanceUID = pydicom.uid.generate_uid() if study_instance_uid is None else study_instance_uid
 
-    ds.add(DataElement(0x00100010, 'AS', str(age).rjust(3, "0") + "Y"))
     ds.add(DataElement(0x00081030, 'LO', study_description))
     ds.add(DataElement(0x0008103E, 'LO', ohif_description))
 
+    ds.PatientAge = str(age).rjust(3, "0") + "Y"
     ds.file_meta.TransferSyntaxUID = ImplicitVRLittleEndian
     ds.Rows = image_matrix.shape[0]
     ds.Columns = image_matrix.shape[1]
