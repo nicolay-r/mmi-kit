bd979a2876639c1606882bdd311dde7a1b884e13
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-03-18 17:01:05 +0000

    Refactoring. Removing non-utilized iter service. Moving code related to the content conversion in the related service_convert.py

--- mmi_kit/service_convert.py
+++ mmi_kit/service_convert.py
@@ -1,13 +1,21 @@
+import numpy as np
 import pydicom
 from pydicom import DataElement
 from pydicom.data import get_testdata_file
 from pydicom.uid import ImplicitVRLittleEndian
+
+from mmi_kit.service_pillow import PillowService
+from mmi_kit.service_pydicom import PyDicomService
 
 
 def nifti_to_dicom(patient_id, patient_name, modality, image_matrix, age,
                    study_description="", ohif_description="",
                    frame_of_reference_uid=None,
                    series_instance_uid=None, sop_instance_uid=None, study_instance_uid=None):
+    """ This is experimental approach. Not recommended to use.
+        The actual implementation is more difficult that this.
+    """
+
     assert (isinstance(age, int) and age > 0)
     assert (isinstance(patient_name, str))
     assert (isinstance(patient_id, str))
@@ -48,3 +56,43 @@ def nifti_to_dicom(patient_id, patient_name, modality, image_matrix, age,
     ds.PixelData = image_matrix.tobytes()
 
     return ds
+
+
+def dicom_to_png(dicom_path, png_path, scale=255.0, extra_brightness=0):
+
+    # Read the DICOM file
+    ds = pydicom.dcmread(dicom_path)
+
+    # Extract the pixel array
+    # Make sure that we convert to the float type to avoid quality loss during further transformations.
+    pixel_array = PyDicomService._try_extract_pixel_data(ds=ds)
+
+    if pixel_array is None:
+        raise Exception(f"Image / Pixel Array is not presented for the DICOM: {dicom_path}")
+
+    pixel_array = pixel_array.astype(np.float64)
+
+    # Extracting window center.
+    window_center = None
+    if 'WindowCenter' in ds:
+        window_center = -ds.WindowCenter[0] \
+            if isinstance(ds.WindowCenter, pydicom.multival.MultiValue) \
+            else -ds.WindowCenter
+
+    # Rescaling back to the original image.
+    if window_center is not None and 'RescaleIntercept' in ds:
+        intercept = window_center + ds.RescaleIntercept + extra_brightness
+        pixel_array += intercept
+
+    # Replace negative values to zero.
+    pixel_array = np.maximum(pixel_array, 0)
+
+    # Contract correction via scale parameter.
+    image = PillowService.from_grayscale_pixeldata(pixel_array, scale=scale)
+
+    # Save the image as PNG and remove the related object.
+    if png_path is not None:
+        image.save(png_path)
+        image.close()
+
+    return image
--- mmi_kit/service_pydicom.py
+++ mmi_kit/service_pydicom.py
@@ -1,9 +1,5 @@
 import warnings
-
-import numpy as np
 import pydicom
-
-from mmi_kit.service_pillow import PillowService
 
 
 class PyDicomService(object):
@@ -55,10 +51,6 @@ class PyDicomService(object):
             raise ValueError("PixelSpacing attribute not found in the DICOM dataset.")
 
     @staticmethod
-    def is_dicom_filepath(filepath):
-        return filepath.split('.')[-1] in ["dicom", "dcm"]
-
-    @staticmethod
     def _iter_metadata_recursive(ds, suppress_wa=False):
 
         with warnings.catch_warnings():
@@ -93,43 +85,3 @@ class PyDicomService(object):
                 ds=pydicom.dcmread(filepath),
                 suppress_wa=suppress_wa)
         }
-
-    @staticmethod
-    def dicom_to_png(dicom_path, png_path, scale=255.0, extra_brightness=0):
-
-        # Read the DICOM file
-        ds = pydicom.dcmread(dicom_path)
-
-        # Extract the pixel array
-        # Make sure that we convert to the float type to avoid quality loss during further transformations.
-        pixel_array = PyDicomService._try_extract_pixel_data(ds=ds)
-
-        if pixel_array is None:
-            raise Exception(f"Image / Pixel Array is not presented for the DICOM: {dicom_path}")
-
-        pixel_array = pixel_array.astype(np.float64)
-
-        # Extracting window center.
-        window_center = None
-        if 'WindowCenter' in ds:
-            window_center = -ds.WindowCenter[0] \
-                if isinstance(ds.WindowCenter, pydicom.multival.MultiValue) \
-                else -ds.WindowCenter
-
-        # Rescaling back to the original image.
-        if window_center is not None and 'RescaleIntercept' in ds:
-            intercept = window_center + ds.RescaleIntercept + extra_brightness
-            pixel_array += intercept
-
-        # Replace negative values to zero.
-        pixel_array = np.maximum(pixel_array, 0)
-
-        # Contract correction via scale parameter.
-        image = PillowService.from_grayscale_pixeldata(pixel_array, scale=scale)
-
-        # Save the image as PNG and remove the related object.
-        if png_path is not None:
-            image.save(png_path)
-            image.close()
-
-        return image
