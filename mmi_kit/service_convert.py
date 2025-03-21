ce5d5ea3b294fb82664354cda5106139d31d4ddc
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-03-21 11:48:40 +0000

    roll-back the default function

--- mmi_kit/service_convert.py
+++ mmi_kit/service_convert.py
@@ -1,5 +1,8 @@
 import numpy as np
 import pydicom
+from pydicom import DataElement
+from pydicom.data import get_testdata_file
+from pydicom.uid import ImplicitVRLittleEndian
 
 from mmi_kit.service_pillow import PillowService
 from mmi_kit.service_pydicom import PyDicomService
@@ -43,3 +46,53 @@ def dicom_to_png(dicom_path, png_path, scale=255.0, extra_brightness=0):
         image.close()
 
     return image
+
+
+def nifti_to_dicom(patient_id, patient_name, modality, image_matrix, age,
+                   study_description="", series_description="",
+                   frame_of_reference_uid=None,
+                   series_instance_uid=None, sop_instance_uid=None, study_instance_uid=None):
+    """ This is experimental approach. Not recommended to use.
+        The actual implementation is more difficult that this.
+    """
+
+    assert (isinstance(age, int) and age > 0)
+    assert (isinstance(patient_name, str))
+    assert (isinstance(patient_id, str))
+    assert (isinstance(modality, str))
+    assert (modality in ["CT", "MR"])
+
+    templates = {
+        "CT": get_testdata_file("CT_small.dcm"),
+        "MR": get_testdata_file("MR_small.dcm")
+    }
+
+    ds = pydicom.dcmread(templates[modality.upper()])
+
+    image_matrix = image_matrix.astype('uint16')
+
+    # Patient related info setup
+    ds.PatientName = patient_name
+    ds.PatientID = patient_id
+    ds.Modality = modality
+    ds.SeriesInstanceUID = pydicom.uid.generate_uid() if series_instance_uid is None else series_instance_uid
+    ds.SOPInstanceUID = pydicom.uid.generate_uid() if sop_instance_uid is None else sop_instance_uid
+    ds.StudyInstanceUID = pydicom.uid.generate_uid() if study_instance_uid is None else study_instance_uid
+    ds.FrameOfReferenceUID = pydicom.uid.generate_uid() if frame_of_reference_uid is None else study_instance_uid
+
+    ds.add(DataElement(0x00081030, 'LO', study_description))
+    ds.add(DataElement(0x0008103E, 'LO', series_description))
+
+    ds.PatientAge = str(age).rjust(3, "0") + "Y"
+    ds.file_meta.TransferSyntaxUID = ImplicitVRLittleEndian
+    ds.Rows = image_matrix.shape[0]
+    ds.Columns = image_matrix.shape[1]
+    ds.PhotometricInterpretation = "MONOCHROME2"
+    ds.SamplesPerPixel = 1
+    ds.BitsStored = 16
+    ds.BitsAllocated = 16
+    ds.HighBit = 15
+    ds.PixelRepresentation = 1
+    ds.PixelData = image_matrix.tobytes()
+
+    return ds
--- test/test_iter_nifti_as_dicoms.py
+++ test/test_iter_nifti_as_dicoms.py
@@ -1,7 +1,8 @@
 from mmi_kit.service_convert import nifti_to_dicom
 from mmi_kit.service_nifti import NiftiService
 
+
 for ind, arr in enumerate(NiftiService.iter_slices(filepath="data/512.nii.gz")):
-    ds = nifti_to_dicom(image_matrix=arr, modality="CT", patient_id="1", patient_name="Anonymous")
+    ds = nifti_to_dicom(image_matrix=arr, modality="CT", patient_id="1", patient_name="Anonymous", age=60)
     ds.save_as(f"out.dcm")
     break
