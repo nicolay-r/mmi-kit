0a9e18f4cbcfe93b6f2c0e42117484a3792ffca6
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-03-24 12:55:23 +0000

    Enhancing zip service

--- mmi_kit/service_zip.py
+++ mmi_kit/service_zip.py
@@ -10,22 +10,39 @@ class ZipService(object):
         return zipfile.ZipFile(zip_filepath, 'r')
 
     @staticmethod
+    def __read_inner_file(zip_file, file_name):
+        with zip_file.open(file_name, mode="r") as file:
+            return io.BytesIO(file.read())
+
+    @staticmethod
     def read_inner_file(zip_filepath, file_name):
         with ZipService.__open(zip_filepath) as zip_file:
-            with zip_file.open(file_name, mode="r") as file:
-                return io.BytesIO(file.read())
+            return ZipService.__read_inner_file(zip_file=zip_file, file_name=file_name)
 
     @staticmethod
-    def __iter_zip_contents(zip_filepath, filter_func):
+    def __iter_zip_contents(zip_file, filter_func):
+        for file_name in zip_file.namelist():
+            if filter_func(file_name):
+                yield file_name
+
+    @staticmethod
+    def __iter_zip_contents_pattern(zip_file, pattern, **kwargs):
+        return ZipService.__iter_zip_contents(zip_file=zip_file,
+                                              filter_func=lambda filename: fnmatch(filename, pattern),
+                                              **kwargs)
+
+    @staticmethod
+    def iter_inner_files(zip_filepath, **kwargs):
         with ZipService.__open(zip_filepath) as zip_file:
-            for file_name in zip_file.namelist():
-                if filter_func(file_name):
-                    yield file_name
+            for file_name in ZipService.__iter_zip_contents_pattern(zip_file=zip_file, **kwargs):
+                yield file_name, ZipService.__read_inner_file(zip_file=zip_file, file_name=file_name)
 
     @staticmethod
-    def iter_zip_contents(**kwargs):
-        return ZipService.__iter_zip_contents(filter_func=lambda _: True, **kwargs)
+    def iter_zip_contents(zip_filepath, **kwargs):
+        with ZipService.__open(zip_filepath) as zip_file:
+            return ZipService.__iter_zip_contents(zip_file=zip_file, filter_func=lambda _: True, **kwargs)
 
     @staticmethod
-    def iter_zip_contents_pattern(pattern, **kwargs):
-        return ZipService.__iter_zip_contents(filter_func=lambda filename: fnmatch(filename, pattern), **kwargs)
+    def iter_zip_contents_pattern(zip_filepath, **kwargs):
+        with ZipService.__open(zip_filepath) as zip_file:
+            return ZipService.__iter_zip_contents_pattern(zip_file=zip_file, **kwargs)
