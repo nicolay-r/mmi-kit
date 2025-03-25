059c31743d160e112303364ac5e24d154bd31141
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-03-25 12:27:53 +0000

    Added json-related service

---
+++ mmi_kit/service_json.py
@@ -0,0 +1,14 @@
+import json
+
+
+class JsonService(object):
+
+    @staticmethod
+    def load(filepath):
+        with open(filepath) as json_data:
+            return json.load(json_data)
+
+    @staticmethod
+    def write(filepath, data, sort_keys=True, indent=4, ensure_ascii=False):
+        with open(filepath, 'w') as out_file:
+            json.dump(data, out_file, sort_keys=sort_keys, indent=indent, ensure_ascii=ensure_ascii)
