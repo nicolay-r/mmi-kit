d5423207ce55ce2a1547fb980a9eb44a18ece4c8
Nicolay Rusnachenko <rusnicolay@gmail.com>
2025-03-31 13:49:17 +0100

    NIFTI: Support matching of the affine and header data from the original file

--- mmi_kit/service_nifti.py
+++ mmi_kit/service_nifti.py
@@ -7,19 +7,20 @@ class NiftiService(object):
     """
 
     @staticmethod
-    def __read_data(filepath, **kwargs):
+    def __read(filepath, **kwargs):
         nifti_img = nib.load(filepath, **kwargs)
-        return nifti_img.get_fdata()
+        nifti_data = nifti_img.get_fdata()
+        return nifti_img, nifti_data
 
     @staticmethod
     def read_image(filepath, **kwargs):
-        return NiftiService.__read_data(filepath, **kwargs)
+        return NiftiService.__read(filepath, **kwargs)
 
     @staticmethod
     def iter_slices(filepath, handle_func=None, handle_kwargs=dict()):
         assert (isinstance(filepath, str))
 
-        nifti_data = NiftiService.__read_data(filepath, **handle_kwargs)
+        _, nifti_data = NiftiService.__read(filepath, **handle_kwargs)
 
         # Iterate over each slice in the z-dimension (axis 2)
         for slice_ind in range(nifti_data.shape[2]):
@@ -37,9 +38,13 @@ class NiftiService(object):
         return header['descrip'].astype(str)
 
     @staticmethod
-    def save_image(array_slices, filename, affine=None, header=None, **kwargs):
-        assert (isinstance(array_slices, np.ndarray))
-        img = nib.Nifti1Image(array_slices,
-                              affine=np.eye(4) if affine is None else affine,
-                              header=header)
-        nib.save(img, filename=filename, **kwargs)
+    def save_image(array_slices, filename, src_nifti=None, **nifti2img_kwargs):
+        assert (isinstance(array_slices, np.narray))
+
+        if src_nifti is not None:
+            img, _ = NiftiService.__read(src_nifti)
+            nifti2img_kwargs["affine"] = img.affine
+            nifti2img_kwargs["header"] = img.header
+
+        img = nib.Nifti1Image(array_slices, **nifti2img_kwargs)
+        nib.save(img, filename=filename)
